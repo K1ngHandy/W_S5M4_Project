047e79552d8ca74a6c27d28d08e993ccea33e046
K1ngHandy <bolds-slicks0g@icloud.com>
2024-10-22 12:06:59 -0400

    change location display in widget

--- frontend/index.js
+++ frontend/index.js
@@ -19,28 +19,43 @@ async function moduleProject4() {
   // 👉 Tasks 1 - 5 go here
     // 1
   const weatherWidget = document.querySelector('#weatherWidget');
+  const today = document.querySelector('#today');
+  // console.log('Today:', today);
+  const forecast = document.querySelector('#forecast');
+  const location = document.querySelector('#location');
+
   weatherWidget.style.display = 'none';
 
     // 2
   const dropdown = document.querySelector('#citySelect');
+
     // 3
   dropdown.addEventListener('change', (event) => {
     let currentCity = event.target.value;
     const info = document.querySelector('.info');
-    
-    // if (priorCity) {
-    //   priorCity.disabled = false;
-    // }
 
     if (typeof axios !== 'undefined') {
       info.innerText = 'Fetching weather data...';
+      dropdown.style.disabled = true;
       weatherWidget.style.display = 'none';
 
       axios.get(`http://localhost:3003/api/weather?city=${currentCity}`)
       .then(res => {
         currentCity.disabled = true;
         const data = res.data;
-        console.log('Fetch data', data);
+        console.log('Fetch data:', data);
+        
+        // today
+        const todayData = data.current;
+        // forecast
+        const forecastData = data.forecast;
+        // location
+        const locationData = data.location;
+        console.log('Location data:', locationData);
+
+        location.children[0].innerText = locationData.city;
+        location.children[1].innerText = locationData.country;
+
         weatherWidget.style.display = 'block';
         console.log(`Current city: ${currentCity}`);
       })
